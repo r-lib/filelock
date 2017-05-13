50eff578d917fe6bbe1befb6d8b738a681487400
Gábor Csárdi <csardi.gabor@gmail.com>
2017-05-13 15:43:00 +0100

    Test cases

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,6 @@
-library(testthat)
-library(filelock)
 
-test_check("filelock")
+if (Sys.getenv("NOT_CRAN", "") != "") {
+  library(testthat)
+  library(filelock)
+  test_check("filelock")
+}
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,8 +1,208 @@
 
 context("filelock")
 
-test_that("filelock works", {
+test_that("can create a shared lock", {
 
-  expect_true(TRUE)
+  tmp <- tempfile()
+  expect_silent({
+    lck <- lock(tmp, exclusive = FALSE)
+    unlock(lck)
+  })
+})
 
+test_that("can create an exclusive lock", {
+
+  tmp <- tempfile()
+  expect_silent({
+    lck <- lock(tmp, exclusive = TRUE)
+    unlock(lck)
+  })
+})
+
+test_that("an exclusive lock really locks", {
+
+  tmp <- tempfile()
+  lck <- lock(tmp, exclusive = TRUE)
+
+  res <- callr::r_safe(
+    function(path) filelock::lock(path, timeout = 0),
+    list(path = tmp),
+    timeout = 1,
+    spinner = FALSE
+  )
+
+  expect_null(res)
+  unlock(lck)
+})
+
+test_that("can release a lock", {
+
+  tmp <- tempfile()
+  lck <- lock(tmp, exclusive = TRUE)
+
+  res <- callr::r_safe(
+    function(path) filelock::lock(path, timeout = 0),
+    list(path = tmp),
+    timeout = 1,
+    spinner = FALSE
+  )
+
+  expect_null(res)
+  unlock(lck)
+
+  res <- callr::r_safe(
+    function(path) filelock::lock(path, timeout = 0),
+    list(path = tmp),
+    timeout = 1,
+    spinner = FALSE
+  )
+
+  ## By the time it gets here, it will be unlocked, because it is
+  ## an external pointer, so we cannot save it to file, and the child
+  ## process finishes, anyway.
+  expect_equal(class(res), "filelock_lock")
+})
+
+test_that("printing the lock", {
+
+  tmp <- tempfile()
+  lck <- lock(tmp, exclusive = TRUE)
+
+  expect_output(print(lck), "Lock on")
+  expect_output(print(lck), basename(normalizePath(tmp)), fixed = TRUE)
+
+  unlock(lck)
+  expect_output(print(lck), "Unlocked lock on")
+  expect_output(print(lck), basename(normalizePath(tmp)), fixed = TRUE)
+})
+
+test_that("finalizer works", {
+
+  tmp <- tempfile()
+  lck <- lock(tmp, exclusive = TRUE)
+
+  rm(lck)
+  gc()
+
+  res <- callr::r_safe(
+    function(path) filelock::lock(path, timeout = 0),
+    list(path = tmp),
+    timeout = 1,
+    spinner = FALSE
+  )
+
+  expect_equal(class(res), "filelock_lock")
+})
+
+test_that("timeout", {
+
+  tmp <- tempfile()
+  lck <- lock(tmp, exclusive = TRUE)
+
+  tic <- Sys.time()
+
+  res <- callr::r_safe(
+    function(path) filelock::lock(path, timeout = 1000),
+    list(path = tmp),
+    timeout = 2,
+    spinner = FALSE
+  )
+
+  tac <- Sys.time()
+
+  expect_null(res)
+  expect_true(tac - tic >= as.difftime(1, units = "secs"))
+})
+
+test_that("timeout 2", {
+
+  tmp <- tempfile()
+
+  px1_opts <- callr::r_process_options(
+    func = function(path) {
+      lck <- filelock::lock(path)
+      Sys.sleep(1)
+      filelock::unlock(lck)
+    },
+    args = list(path = tmp)
+  )
+  px1 <- callr::r_process$new(px1_opts)
+
+  px2_opts <- callr::r_process_options(
+    func = function(path) filelock::lock(path, timeout = 2000),
+    args = list(path = tmp)
+  )
+  px2 <- callr::r_process$new(px2_opts)
+
+  px2$wait(timeout = 3000)
+  if (! px2$is_alive()) {
+    res <- px2$get_result()
+    expect_equal(class(res), "filelock_lock")
+  } else {
+    px1$kill()
+    px2$kill()
+    stop("Process did not finish, something is wrong")
+  }
+})
+
+test_that("wait forever", {
+
+  tmp <- tempfile()
+
+  px1_opts <- callr::r_process_options(
+    func = function(path) {
+      lck <- filelock::lock(path)
+      Sys.sleep(10)
+    },
+    args = list(path = tmp)
+  )
+  px1 <- callr::r_process$new(px1_opts)
+
+  px2_opts <- callr::r_process_options(
+    func = function(path) filelock::lock(path, timeout = Inf),
+    args = list(path = tmp)
+  )
+  px2 <- callr::r_process$new(px2_opts)
+
+  px2$wait(timeout = 1000)
+  if (px2$is_alive()) {
+    px1$kill()
+    px2$kill()
+    expect_true(px2$get_exit_status() < 0)
+  } else {
+    px1$kill()
+    px2$kill()
+    stop("Process already done, something is wrong")
+  }
+})
+
+test_that("wait forever, lock released", {
+
+  tmp <- tempfile()
+
+  ## This process just finishes normally, and that releases the lock
+  px1_opts <- callr::r_process_options(
+    func = function(path) {
+      lck <- filelock::lock(path)
+      Sys.sleep(1)
+    },
+    args = list(path = tmp)
+  )
+  px1 <- callr::r_process$new(px1_opts)
+
+  px2_opts <- callr::r_process_options(
+    func = function(path) filelock::lock(path, timeout = Inf),
+    args = list(path = tmp)
+  )
+  px2 <- callr::r_process$new(px2_opts)
+
+  px2$wait(timeout = 3000)
+  if (! px2$is_alive()) {
+    res <- px2$get_result()
+    expect_equal(class(res), "filelock_lock")
+  } else {
+    px1$kill()
+    px2$kill()
+    stop("Process did not finish, something is wrong")
+  }
 })
